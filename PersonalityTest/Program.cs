412702962161e55b48610077a7212a80e69cda24
unknown <ikorpan@tktk.ee>
2021-09-19 23:53:39 +0300

    add new projects

---
+++ PersonalityTest/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace PersonalityTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm kysib kasutajal sisestada tema lemmik v@rv;
+            //kui kasutaja sisestab punane, konsol kuvab 'oled romantiline';
+            //kui kasutaja sisestab sinine, konsol kuvab 'oled tookas';
+            //roheline, loodus s6ber;
+            //muud- oled {user color} ykssarvik;
+
+            Console.WriteLine("Sisesta oma lemmik v@rv:");
+            string UserColor = Console.ReadLine();
+
+            if(UserColor == "punane")
+            {
+                Console.WriteLine("Oled romantiline.");
+            }
+            else if (UserColor == "sinine")
+            {
+                Console.WriteLine("Oled töökas.");
+            }
+            else if (UserColor == "roheline")
+            {
+                Console.WriteLine("Oled Ecoaktivist.");
+            }
+
+            else
+            {
+                Console.WriteLine($"Oled {UserColor} ükssarvik ");
+
+            }
+
+            Console.WriteLine("good day!");
+        }
+    }
+}
---
+++ PersonalityTestSwitch/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace PersonalityTestSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm kysib kasutajal sisestada tema lemmik v@rv;
+            //kui kasutaja sisestab punane, konsol kuvab 'oled romantiline';
+            //kui kasutaja sisestab sinine, konsol kuvab 'oled tookas';
+            //roheline, loodus s6ber;
+            //muud- oled {user color} ykssarvik;
+           
+            
+            Console.WriteLine("Sisesta oma lemmik v@rv:");
+            string UserColor = Console.ReadLine().ToLower();
+
+            //Suur t@ht v6ib olla;
+
+
+            switch (UserColor)
+
+            {
+                case "punane":
+                    Console.WriteLine("Oled Kommunist");
+                    break;
+                case "sinine":
+                    Console.WriteLine("Oled Liberaal");
+                    break;
+                case "roheline":
+                    Console.WriteLine("Oled EcoAktivist");
+                    break;
+                default:
+                    Console.WriteLine($"Oled {UserColor} ykssarvik");
+                    break;
+            }
+
+
+            Console.WriteLine("GOOD BUY, TOVARISH");
+
+        }
+    }
+}
